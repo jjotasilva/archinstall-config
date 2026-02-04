eccd6629e61a868fe54f15eb143976a5ad33cfe1
jjotasilva <141449331+jjotasilva@users.noreply.github.com>
2026-02-04 13:21:20 -0300

    Update 03_configurations.sh

--- configs/03_configurations.sh
+++ configs/03_configurations.sh
@@ -71,6 +71,7 @@ fi
 sudo systemctl enable --now fstrim.timer
 sudo systemctl enable --now firewalld.service
 sudo systemctl enable --now power-profiles-daemon.service
+sudo systemctl enable --now systemd-resolved
 sudo systemctl enable --now systemd-timesyncd
 systemctl --user enable --now gamemoded
 
